38d91777d4fa225a3bf9d7eebcf27754053b1b79
Aungkokolin1997 <lin@quartile.co>
2024-11-06 10:59:32 +0000

    [ADD] l10n_jp_summary_invoice

---
+++ l10n_jp_summary_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "JP Summary Invoice",
+    "version": "16.0.1.0.0",
+    "category": "l10n",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": [
+        "account_billing",
+    ],
+    "data": [
+        "views/account_billing_views.xml",
+    ],
+    "installable": True,
+}
