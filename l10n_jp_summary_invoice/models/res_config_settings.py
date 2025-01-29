c1455b321f33eb999a4e050a4b49596bfbc89abb
Yoshi Tashiro <tashiro@quartile.co>
2025-01-29 09:15:03 +0000

    add header remark field

---
+++ l10n_jp_summary_invoice/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    summary_invoice_remark = fields.Html(
+        related="company_id.summary_invoice_remark", readonly=False
+    )
